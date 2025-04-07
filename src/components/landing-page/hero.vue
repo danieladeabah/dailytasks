5eaf7e30fb77f2750ec0d2918a0c30e90cfecc7b
danieladeabah <daniel.adeabah@amalitech.com>
2025-04-07 00:27:52 +0000

    Fix text in hero component for clarity and improve formatting

--- src/components/landing-page/hero.vue
+++ src/components/landing-page/hero.vue
@@ -1,13 +1,11 @@
 <template>
   <section class="bg-gradient-to-r from-red-500 to-blue-300 py-16 md:p-16">
-    <div
-      class="container mx-auto flex flex-col items-center justify-between md:flex-col lg:flex-row"
-    >
+    <div class="container mx-auto flex flex-col items-center justify-between md:flex-col lg:flex-row">
       <div class="text-center lg:pr-8 lg:text-left">
         <h1 class="text-4xl font-bold leading-snug text-white md:text-5xl">
           Simplify Your Day
           <p class="my-4 text-yellow-200">
-            Create ./dailyTasks
+            Create Tasks
             <span class="sm:block md:hidden lg:block">Effortlessly</span>
           </p>
         </h1>
@@ -16,25 +14,17 @@
           <li>Stay organized and boost your productivity with ease.</li>
         </ul>
         <div>
-          <NuxtLink
-            :to="isLoggedIn ? '/dashboard' : '/auth/login'"
-            class="mt-8 inline-block rounded-md bg-yellow-300 px-6 py-3 font-semibold text-black transition hover:bg-yellow-400"
-          >
+          <NuxtLink :to="isLoggedIn ? '/dashboard' : '/auth/login'"
+            class="mt-8 inline-block rounded-md bg-yellow-300 px-6 py-3 font-semibold text-black transition hover:bg-yellow-400">
             {{ isLoggedIn ? 'Go to Dashboard' : 'Get Started Now' }}
           </NuxtLink>
         </div>
       </div>
       <div class="mt-8 flex gap-4 md:ml-8 md:mt-8">
-        <img
-          src="/assets/icons/mobile-dashboard.png"
-          alt="Task management"
-          class="h-[20rem] w-[10rem] rounded-md object-cover"
-        />
-        <img
-          src="/assets/icons/mobile-viewed-tasks.png"
-          alt="Task management"
-          class="h-[20rem] w-[10rem] rounded-md object-cover"
-        />
+        <img src="/assets/icons/mobile-dashboard.png" alt="Task management"
+          class="h-[20rem] w-[10rem] rounded-md object-cover" />
+        <img src="/assets/icons/mobile-viewed-tasks.png" alt="Task management"
+          class="h-[20rem] w-[10rem] rounded-md object-cover" />
       </div>
     </div>
   </section>
