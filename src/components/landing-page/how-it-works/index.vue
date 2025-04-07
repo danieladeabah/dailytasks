485005150cfcb46a4a3a3f7beb282893abc1203a
danieladeabah <daniel.adeabah@amalitech.com>
2025-04-07 00:27:40 +0000

    Refactor iframe attributes and styling for improved readability

--- src/components/landing-page/how-it-works/index.vue
+++ src/components/landing-page/how-it-works/index.vue
@@ -6,22 +6,15 @@
         Learn how to use our task management solution.
       </p>
     </div>
-    <div
-      style="
+    <div style="
         position: relative;
         padding-bottom: calc(56.25% + 41px);
         height: 0;
         width: 100%;
-      "
-    >
+      ">
       <iframe
         src="https://demo.arcade.software/l4Q3vVW3TeM4SrwAVErX?embed&embed_mobile=inline&embed_desktop=tab&show_copy_link=true"
-        title="./dailyTasks"
-        loading="lazy"
-        webkitallowfullscreen
-        mozallowfullscreen
-        allowfullscreen
-        allow="clipboard-write"
+        title="Tasks" loading="lazy" webkitallowfullscreen mozallowfullscreen allowfullscreen allow="clipboard-write"
         style="
           position: absolute;
           top: 0;
@@ -29,8 +22,7 @@
           width: 100%;
           height: 100%;
           color-scheme: light;
-        "
-      ></iframe>
+        "></iframe>
     </div>
   </UiKitsUiSlotsProjectActivitySlot>
 </template>
